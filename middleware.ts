03c42647050a43a5a0fa5d9673411993b97cacb3
Izzy Mansurov <ismatullamansurov@gmail.com>
2024-10-13 18:30:33 -0600

    fix: use shadcn components for login

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
-import { type NextRequest } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server';
 import { updateSession } from './src/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
+  // Redirect to login page if the request is for the root path
+  if (request.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  // Proceed with session update if not redirecting
   return await updateSession(request);
 }
 
